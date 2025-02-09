7202109198e3b722de81b774d60f6fbd426d868d
Evanthia <epalatou@outlook.com>
2025-02-10 00:22:32 +0200

    readies code for deploy

--- car_rental/urls.py
+++ car_rental/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from hello_world import views as index_views
 from about import views as about_views
+from cars.views import my_cars
 
 urlpatterns = [
     path('hello/', index_views.index, name='index'),
     path('about/', about_views.about_me, name='about'),
     path('admin/', admin.site.urls),
+    path('cars/', my_cars, name='cars')
 ]
