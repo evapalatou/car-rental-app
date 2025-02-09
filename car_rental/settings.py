ce0ea0bbaa7cfd6625d4abac65d3787da2c232dc
Evanthia <epalatou@outlook.com>
2025-02-09 13:33:19 +0200

    add about app to test django environment

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+-4^q%l!ani#h#_r2r*+679$i5w@=!agni5n)w6w@v!-56k1q7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
